463fdf84c51d9c813dbbccc07216d063beab28a4
aco401 <tobias.westphal@haw-hamburg.de>
2022-02-25 13:51:02 +0100

    Adjust to bench usage

---
+++ dijkstra/dijkstra.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2019 Mesotic SAS
+ *               2020 Gunar Schorcht
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     drivers_bme680
+ *
+ * @{
+ * @file
+ * @brief       Default configuration for BME680 device driver
+ *
+ * @author      Dylan Laduranty <dylan.laduranty@mesotic.com>
+ * @author      Gunar Schorcht <gunar@schorcht.net>
+ */
+
+#ifndef MIBENCH2_DIJKSTRA
+#define MIBENCH2_DIJKSTRA
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void dijkstra_bench(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MIBENCH2_DIJKSTRA */
