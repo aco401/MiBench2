90610c018142ed360e47d2d316a929a9da1632dd
aco401 <tobias.westphal@haw-hamburg.de>
2022-02-26 12:49:32 +0100

    Remove header

--- dijkstra/dijkstra_bench.h
+++ dijkstra/dijkstra_bench.h
@@ -1,10 +1,5 @@
 /*
- * Copyright (C) 2019 Mesotic SAS
- *               2020 Gunar Schorcht
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
+ * Copyright (C) 
  */
 
 #ifndef MIBENCH2_DIJKSTRA
