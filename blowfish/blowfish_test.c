de61d1edabc21ff3320b20bd27d527ffc04a4432
aco401 <tobias.westphal@haw-hamburg.de>
2022-04-24 23:15:16 +0200

    blowfish: Adjust for RIOT package usage

--- blowfish/blowfish_test.c
+++ blowfish/blowfish_test.c
@@ -23,42 +23,51 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 unsigned char KEY[] = "1234567890abcdeffedcba0987654321";
 
-int main(void) {
+int blowfish_bench(void) {
   unsigned long L = 1, R = 2;
   BLOWFISH_CTX ctx;
 
   Blowfish_Init (&ctx, (unsigned char*)"TESTKEY", 7);
 
   Blowfish_Encrypt(&ctx, &L, &R);
+  #ifdef LOG_DEBUG
   printf("%08lX %08lX\n", L, R);
   if (L == 0xDF333FD2L && R == 0x30A71BB4L)
 	  printf("Test encryption OK.\n");
   else
 	  printf("Test encryption failed.\n");
+  #endif
 
   Blowfish_Decrypt(&ctx, &L, &R);
+  #ifdef LOG_DEBUG
   if (L == 1 && R == 2)
   	  printf("Test decryption OK.\n");
   else
 	  printf("Test decryption failed.\n");
     
   printf("Encrypt message\n");
+  #endif
   Blowfish_Init (&ctx, KEY, sizeof(KEY));
 
   unsigned long * plaintextPtr = (unsigned long *)test_data;
   while(plaintextPtr < (unsigned long *)(test_data + sizeof(test_data))) {
       Blowfish_Encrypt(&ctx, &plaintextPtr[0], &plaintextPtr[1]);
+      #ifdef LOG_DEBUG
       printf("%08lX %08lX\n", plaintextPtr[0], plaintextPtr[1]);
+      #endif
       plaintextPtr += 2;
   }
-
+  #ifdef LOG_DEBUG
   printf("Decrypt message\n");
+  #endif
   Blowfish_Init (&ctx, KEY, sizeof(KEY));
     
   plaintextPtr = (unsigned long *)test_data;
   while(plaintextPtr < (unsigned long *)(test_data + sizeof(test_data))) {
       Blowfish_Decrypt(&ctx, &plaintextPtr[0], &plaintextPtr[1]);
+      #ifdef LOG_DEBUG
       printf("%08lX %08lX\n", plaintextPtr[0], plaintextPtr[1]);
+      #endif
       plaintextPtr += 2;
   }
     
