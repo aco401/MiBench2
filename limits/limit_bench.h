e8bb5a0540a713c4093e2851f420b4ad8e6dfede
aco401 <tobias.westphal@haw-hamburg.de>
2022-04-25 14:06:09 +0200

    limit: Adjust for riot usage

---
+++ limits/limit_bench.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2019 Mesotic SAS
+ *               2020 Gunar Schorcht
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#ifndef MIBENCH2_LIMIT
+#define MIBENCH2_LIMIT
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int limit_bench(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MIBENCH2_LIMIT */
--- limits/limit_test.c
+++ limits/limit_test.c
@@ -1,4 +1,4 @@
-
+#ifdef LOG_DEBUG
 void PUT32 ( unsigned int, unsigned int);
 
 #define THUL_UART_BASE 0xE0000000
@@ -26,6 +26,7 @@ void hexstring ( unsigned int d )
     uart_putc(0x0D);
     uart_putc(0x0A);
 }
+#endif
 
 extern unsigned short ult ( unsigned short a, unsigned short b);
 extern unsigned short slt ( short a, short b);
@@ -46,67 +47,97 @@ unsigned short limit_test ( void )
     short sa;
 
     for(ra=0;;ra++) if(ult(ra,7)==0) break;
+    #ifdef LOG_DEBUG
     hexstring(ra);
+    #endif
     if(ra!=7) return(1);
 
     for(ra=0xF000;;ra++) if(ult(ra,0xF007)==0) break;
+    #ifdef LOG_DEBUG
     hexstring(ra);
+    #endif
     if(ra!=0xF007) return(1);
 
     for(sa=-7;;sa++) if(slt(sa,7)==0) break;
+    #ifdef LOG_DEBUG
     hexstring(sa);
+    #endif
     if(sa!=7) return(1);
 
     for(sa=-17;;sa++) if(slt(sa,-7)==0) break;
+    #ifdef LOG_DEBUG
     hexstring(sa);
+    #endif
     if(sa!=-7) return(1);
 
     for(ra=0;;ra++) if(ulte(ra,7)==0) break;
+    #ifdef LOG_DEBUG
     hexstring(ra);
+    #endif
     if(ra!=8) return(1);
 
     for(ra=0xF000;;ra++) if(ulte(ra,0xF007)==0) break;
+    #ifdef LOG_DEBUG
     hexstring(ra);
+    #endif
     if(ra!=0xF008) return(1);
 
     for(ra=0xF000;;ra++) if(ulte(ra,7)==0) break;
+    #ifdef LOG_DEBUG
     hexstring(ra);
+    #endif
     if(ra!=0xF000) return(1);
 
     for(sa=-7;;sa++) if(slte(sa,7)==0) break;
+    #ifdef LOG_DEBUG
     hexstring(sa);
+    #endif
     if(sa!=8) return(1);
 
     for(sa=-17;;sa++) if(slte(sa,-7)==0) break;
+    #ifdef LOG_DEBUG
     hexstring(sa);
+    #endif
     if(sa!=-6) return(1);
 
     for(ra=0;;ra++) if(ugt(ra,7)) break;
+    #ifdef LOG_DEBUG
     hexstring(ra);
+    #endif
     if(ra!=8) return(1);
 
     for(ra=0xF000;;ra++) if(ugt(ra,0xF007)) break;
+    #ifdef LOG_DEBUG
     hexstring(ra);
+    #endif
     if(ra!=0xF008) return(1);
 
     for(ra=0;;ra++) if(ugte(ra,7)) break;
+    #ifdef LOG_DEBUG
     hexstring(ra);
+    #endif
     if(ra!=7) return(1);
 
     for(ra=0xF000;;ra++) if(ugte(ra,0xF007)) break;
+    #ifdef LOG_DEBUG
     hexstring(ra);
+    #endif
     if(ra!=0xF007) return(1);
 
     return(0);
 
 }
-unsigned short main ( void )
+int limit_bench ( void )
 {
+    #ifdef LOG_DEBUG
     hexstring(0x1234);
-
     if(limit_test()) hexstring(0xBAD);
-
+    #else
+    limit_test();
+    #endif
+    #ifdef LOG_DEBUG
     hexstring(0x1234);
+    #endif
     return(0);
 }
 
