828467cca786259d21dc9cb84165b355600845ff
aco401 <tobias.westphal@haw-hamburg.de>
2022-04-25 14:00:32 +0200

    lzfx: Adjust for riot usage

---
+++ lzfx/lzfx_bench.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2019 Mesotic SAS
+ *               2020 Gunar Schorcht
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#ifndef MIBENCH2_LZFX
+#define MIBENCH2_LZFX
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int lzfx_bench(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MIBENCH2_LZFX */
--- lzfx/lzfx_decomp_test.c
+++ lzfx/lzfx_decomp_test.c
@@ -1,9 +1,10 @@
 
 #include "def_data.h"
 
-
+#ifdef LOG_DEBUG
 extern void uart_init ( void );
 extern void hexstring ( unsigned short d );
+#endif
 
 
 int lzfx_decompress_tiny
@@ -25,23 +26,33 @@ int lzfx_decompress_test ( void )
     inf_data_len=sizeof(inf_data);
     ret=lzfx_decompress_tiny(def_data,DEF_DATA_LEN,inf_data,&inf_data_len);
     if(ret) return(ret);
+    #ifdef LOG_DEBUG
     hexstring(inf_data_len);
+    #endif
     if(inf_data_len!=INF_DATA_LEN) return(1);
     rb=0; for(ra=0;ra<inf_data_len;ra++) rb+=inf_data[ra]; rb&=0xFFFF;
+    #ifdef LOG_DEBUG
     hexstring(rb);
+    #endif
     if(rb!=INF_DATA_CHECKSUM) return(1);
     return(0);
 }
 
-int main ( void )
+int lzfx_bench ( void )
 {
+    #ifdef LOG_DEBUG
     uart_init();
     hexstring(0x1234);
+    #endif
     if(lzfx_decompress_test())
     {
+        #ifdef LOG_DEBUG
         hexstring(0x0BAD);
+        #endif
     }
+    #ifdef LOG_DEBUG
     hexstring(0x1234);
+    #endif
     return(0);
 }
 
